6f325960b24397ba1922688097704e658401149b
Adam Sharp <adsharp@me.com>
2015-01-13 07:35:32 +1100

    Add canned URL response for GitHub repositories request

--- HubList/Tests/HubListTests.m
+++ HubList/Tests/HubListTests.m
@@ -7,8 +7,14 @@
 SPEC_BEGIN(HubListTests)
 
 describe(@"HubList", ^{
-    it(@"wins at everything", ^{
-        [[@(2 + 2) should] equal:@4];
+    it(@"can fetch a canned URL", ^{
+        NSURLSession *session = [NSURLSession sharedSession];
+        NSURL *url = [NSURL URLWithString:@"https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc"];
+        NSData __block *receivedData;
+        [[session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            receivedData = data;
+        }] resume];
+        [[expectFutureValue(receivedData) shouldEventually] beNonNil];
     });
 });
 
