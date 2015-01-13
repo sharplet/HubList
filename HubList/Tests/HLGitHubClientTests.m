026f235f1690ef2c8d854d73677d5db170126fb0
Adam Sharp <adsharp@me.com>
2015-01-13 20:43:34 +1100

    Extract helper methods in GitHub client tests

--- HubList/Tests/HLGitHubClientTests.m
+++ HubList/Tests/HLGitHubClientTests.m
@@ -4,9 +4,17 @@
 
 #import <Kiwi/Kiwi.h>
 #import <ILTesting/ILCannedURLProtocol.h>
+#import <EnumeratorKit/EnumeratorKit.h>
 #import "Helpers.h"
 #import "HLGitHubClient.h"
 #import "HLGitHubRepository.h"
+
+// Helpers
+
+NSUInteger numberOfRepositoryInstances(NSArray *repositories);
+
+
+// Spec
 
 SPEC_BEGIN(HLGitHubClientTests)
 
@@ -29,25 +37,25 @@ describe(@"HLGitHubClient", ^{
     describe(@"top repositories", ^{
         it(@"returns an array of the top swift repositories", ^{
             HLGitHubClient *client = [HLGitHubClient new];
+
             NSArray __block *fetchedRepositories;
             [client fetchTopRepositoriesWithCompletion:^(NSArray *topRepositories, NSError *error) {
                 fetchedRepositories = topRepositories;
             }];
 
-            NSUInteger (^numberOfRepositoryInstances)(void) = ^{
-                NSUInteger __block count = 0;
-                for (id repository in fetchedRepositories) {
-                    if ([repository isKindOfClass:[HLGitHubRepository class]]) {
-                        count++;
-                    }
-                }
-                return count;
-            };
-
-            [[expectFutureValue(fetchedRepositories) shouldEventually] beNonNil];
-            [[expectFutureValue(fetchedRepositories) shouldEventually] haveCountOf:numberOfRepositoryInstances()];
+            [[expectFutureValue(fetchedRepositories) shouldEventually] haveCountOfAtLeast:1];
+            [[expectFutureValue(fetchedRepositories) shouldEventually] haveCountOf:numberOfRepositoryInstances(fetchedRepositories)];
         });
     });
 });
 
 SPEC_END
+
+
+// Helper implementations
+
+NSUInteger numberOfRepositoryInstances(NSArray *repositories) {
+    return [repositories filter:^(id repository){
+        return [repository isKindOfClass:[HLGitHubRepository class]];
+    }].count;
+};
